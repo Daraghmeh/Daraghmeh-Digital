f799e6b76ee4384ad84cecb727dc92e9588c7951
Mohammad Daraghmeh <daraghmeh.mohammad1992@gmail.com>
2016-01-09 01:11:31 -0500

    Test123

--- assets/js/hallooou.js
+++ assets/js/hallooou.js
@@ -255,7 +255,27 @@ $(function() {
 
 });
 
-$("#contactForm").validate({
+
+// Validates the contact form and submits it using Formspree.
+$.validator.setDefaults({
+    highlight: function(element) {
+        $(element).closest('.form-group').addClass('has-error');
+    },
+    unhighlight: function(element) {
+        $(element).closest('.form-group').removeClass('has-error');
+    },
+    errorElement: 'span',
+    errorClass: 'help-block',
+    errorPlacement: function(error, element) {
+        if(element.parent('.input-group').length) {
+            error.insertAfter(element.parent());
+        } else {
+            error.insertAfter(element);
+        }
+    }
+});
+
+$("#contact-form").validate({
   submitHandler: function(form) {
     $.ajax({
       url: "//formspree.io/mohammad@daraghmehdigital.com", 
