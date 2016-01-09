de91dde172de02ac19647689fc0aedbb150b7e72
Mohammad Daraghmeh <daraghmeh.mohammad1992@gmail.com>
2016-01-09 01:15:57 -0500

    Fixed Phone
    Added back Phone in Form.

--- assets/js/hallooou.js
+++ assets/js/hallooou.js
@@ -283,7 +283,7 @@ $("#contact-form").validate({
       data: {
         name: $(form).find("input[name='name']").val(),
         email: $(form).find("input[name='email']").val(),
-        phone: $(form).find("input[name='_phone']").val(),
+        phone: $(form).find("input[name='phone']").val(),
         message: $(form).find("textarea[name='message']").val()
       },
       dataType: "json",
