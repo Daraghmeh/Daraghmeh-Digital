eb516df7ea9f83ab111a33de7687fe5505fcb6cb
Mohammad Daraghmeh <daraghmeh.mohammad1992@gmail.com>
2016-01-09 01:07:10 -0500

    Test Me

--- assets/js/hallooou.js
+++ assets/js/hallooou.js
@@ -255,100 +255,24 @@ $(function() {
 
 });
 
-
-
-// Contact form
-$(function() {
-    // validate contact form on keyup and submit
-    $("#contactForm").validate({
-        rules: {
-            name: {
-                required: true,
-                minlength: 2,
-                lettersonly: true
-            },
-            email: {
-                required: true,
-                minlength: 6,
-                email: true
-            },
-            phone: {
-                required: true,
-                digits: true,
-                minlength: 10,
-                maxlength: 15
-            },
-            message: {
-                required: true,
-                minlength: 6
-            }
-        },
-        messages: {
-            name: {
-                required: "Please enter your name",
-                minlength: "Minimum 2 characters",
-                lettersonly: "Only letters please!"
-            },
-            email: {
-                required: "Please enter your email address",
-                minlength: "Minimum 6 characters",
-                email: "That's an invalid email"
-            },
-            phone: {
-                required: "Please enter your phone number",
-                digits: "Only digits (no spaces)",
-                minlength: "Minimum 10 characters",
-                maxlength: "Maximum 15 characters"
-            },
-            message: {
-                required: "Please enter your message",
-                minlength: "Minimum 6 characters"
-            }
-        },
-        success: function(label) {
-            label.addClass("valid").text("Perfect!");
-        },
-        submitHandler: function(element) {
-
-            var ajaxform = $(element),
-                url = ajaxform.attr('action'),
-                type = ajaxform.attr('method'),
-                data = {};
-
-            $(ajaxform).find('[name="submit"]').html('<i class="fa fa-circle-o-notch fa-spin fa-fw"></i> Sending...');
-
-
-            ajaxform.find('[name]').each(function(index, value) {
-                var field = $(this),
-                    name = field.attr('name'),
-                    value = field.val();
-
-                data[name] = value;
-
-            });
-
-            $.ajax({
-                url: url,
-                type: type,
-                data: data,
-                success: function(response) {
-                    if (response.type == 'success') {
-                        $("#contactForm").before("<div class='alert alert-success' role='alert'><a href='#' class='close' data-dismiss='alert'>&times;</a>" + response.text + "</div>");
-                        $(ajaxform).each(function() {
-                            this.reset();
-                            $(this).find('[name="submit"]').html('<i class="fa fa-paper-plane fa-fw"></i> Send');
-                        }).find('.valid').each(function() {
-                            $(this).remove('label.valid');
-                        })
-                    } else if (response.type == 'error') {
-                        $("#contactForm").before("<div class='alert alert-danger' role='alert'><a href='#' class='close' data-dismiss='alert'>&times;</a>" + response.text + "</div>");
-                        $(ajaxform).find('[name="submit"]').html('<i class="fa fa-paper-plane fa-fw"></i> Send');
-                    }
-                }
-            });
-
-            return false;
-        }
+$("#contactForm").validate({
+  submitHandler: function(form) {
+    $.ajax({
+      url: "//formspree.io/mohammad@daraghmehdigital.com", 
+      method: "POST",
+      data: {
+        name: $(form).find("input[name='name']").val(),
+        _replyto: $(form).find("input[name='_replyto']").val(),
+        message: $(form).find("textarea[name='message']").val()
+      },
+      dataType: "json",
+      success: function() {
+        $("#submit-success").fadeIn();
+        $("#contact-form").fadeOut();
+      },
+      error: function() {
+        $("#submit-errors").fadeIn();        
+      }
     });
-
+  }
 });
