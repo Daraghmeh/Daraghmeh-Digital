d89be50055eadd0574e22a4989a74e7e8f21e6da
Mohammad Daraghmeh <daraghmeh.mohammad1992@gmail.com>
2016-01-10 01:11:44 -0500

    Corrected WOW Initialization
    Corrected WOW Initialization vai DD.js.

--- assets/js/DD.js
+++ assets/js/DD.js
@@ -1,3 +1,19 @@
+// WOW.js initialise
+// WOW.js uses animate.css to animate/reveal elements.
+// Browse the list of animation effects available here-> https://daneden.github.io/animate.css/
+$(function() {
+    wow = new WOW(
+    {
+      boxClass:     'wow',      // default
+      animateClass: 'animated', // default
+      offset:       0,          // default
+      mobile:       true,       // default
+      live:         true        // default
+    }
+  )
+  wow.init();
+});
+
 // Recent Project Carousel
 $(function() {
 
