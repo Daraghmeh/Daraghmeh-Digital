027a09cc024ecadca632832cfe119dae3f8d4069
Mohammad Daraghmeh <daraghmeh.mohammad1992@gmail.com>
2016-01-10 23:53:14 -0500

    Clients Remade
    Clients Remade.

--- assets/js/DD.js
+++ assets/js/DD.js
@@ -142,7 +142,7 @@ $(function() {
         image: {
             tError: '<a href="%url%">The image #%curr%</a> could not be loaded.',
             titleSrc: function(item) {
-                return item.el.attr('title') + '<small>by Hallooou</small>';
+                return item.el.attr('title');
             }
         }
     });
